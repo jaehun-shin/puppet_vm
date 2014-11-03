7be1a57aa5996ef6c6117492f3cfeee3ca9dccfd
Shin Jaehun <shinjaehun@linuxmaster.com>
2014-11-03 20:06:06 +0900

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,13 @@ node 'losttemple.linuxmaster.com' {
 #	include sys_info
 }
 
+node 'vm01.linuxmaster.com' {
+    file {'/tmp/testserver':
+        content => "This is a test server.\n",
+    }
+}
+
 node /^vm[0-9]+\.linuxmaster\.com$/ {
-    include vim_upgrade
-    include sshd
+#    include vim_upgrade
+#    include sshd
 }
